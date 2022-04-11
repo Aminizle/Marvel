ac1f28cf240d4c0a3d67c9df9d7444ac80828905
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2022-04-11 18:40:01 -0400

    added api

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
-document.querySelector('.btn').addEventListener('click', getMarvel() )
+document.querySelector('button').addEventListener('click', getMarvel)
+window.onload = getMarvel
+
 
 function getMarvel(){
-fetch(`https://mcuapi.herokuapp.com/api/v1/movies`)
-.then(  res => res.json())
-.then(data => {
-    console.log(data)
+let index = 18
+
+
+fetch(`https://mcuapi.herokuapp.com/api/v1/movies/${index}`)
+.then( res => res.json())
+    .then( data => {
+        console.log(data)
+        document.querySelector('img').src = data.cover_url
+        document.querySelector('.description').innerText = data.overview
+        // document.querySelector('.video').src = data.trailer_url
+        document.querySelector('.mTitle').innerText = data.title
+        document.querySelector('.release').innerText = data.release_date
+        document.querySelector('.directed').innerText = `Directed by: ${data.directed_by}`
+        
 });
 }
+
