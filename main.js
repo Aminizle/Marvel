a6b11d6180931f0ab4c0a7e3ceb98616e3b4dbc3
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2022-04-12 17:58:49 -0400

    added back and forward logic

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
-document.querySelector('button').addEventListener('click', getMarvel)
-window.onload = getMarvel
+document.querySelector('.btn').addEventListener('click', getMarvel)
+document.querySelector('.backBtn').addEventListener('click', lastMarvel)
+document.querySelector('.forwardBtn').addEventListener('click', nextMarvel)
+window.onload = getMarvel //starts the user on their journey
 
+
+let index = 1 //start at the first movie
+
+function lastMarvel(){
+if(index>1 && index<30){ //prevents the user from going beyond the first & last movie
+    index--
+    getMarvel(index)
+}}
+
+function nextMarvel(){
+    if(index>0 && index<29){ 
+        index++
+        getMarvel(index)
+}}
 
 function getMarvel(){
-let index = 18
 
 
 fetch(`https://mcuapi.herokuapp.com/api/v1/movies/${index}`)
@@ -11,12 +26,14 @@ fetch(`https://mcuapi.herokuapp.com/api/v1/movies/${index}`)
     .then( data => {
         console.log(data)
         document.querySelector('img').src = data.cover_url
-        document.querySelector('.description').innerText = data.overview
-        // document.querySelector('.video').src = data.trailer_url
+        document.querySelector('.description').innerText = data.overview        
         document.querySelector('.mTitle').innerText = data.title
-        document.querySelector('.release').innerText = data.release_date
+        document.querySelector('.release').innerText = `Release date: ${data.release_date}`
         document.querySelector('.directed').innerText = `Directed by: ${data.directed_by}`
-        
+        document.querySelector('iframe').src = data.trailer_url        
+        document.querySelector('.imdb').innerText = `IMDB`
+        document.querySelector('.imdb').href = `https://www.imdb.com/title/${data.imdb_id}`
+
 });
 }
 
