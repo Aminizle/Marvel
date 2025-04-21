5be828819b05e6365d320c10da9fa2cdf4dd0722
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2025-04-20 23:54:07 -0400

    added first movie & lastest movie buttons

--- main.js
+++ main.js
@@ -1,41 +1,80 @@
+let index = 1; // Start at the first movie
+let maxIndex = 40; // This will be dynamically set on load
 
-document.querySelector('.backBtn').addEventListener('click', lastMarvel)
-document.querySelector('.forwardBtn').addEventListener('click', nextMarvel)
-window.onload = getMarvel //starts the user on their journey
+// Cache DOM elements
+const backBtn = document.querySelector('.backBtn');
+const forwardBtn = document.querySelector('.forwardBtn');
 
+document.querySelector('.firstBtn').addEventListener('click', firstMarvel);
+document.querySelector('.lastBtn').addEventListener('click', latestMarvel);
+backBtn.addEventListener('click', lastMarvel);
+forwardBtn.addEventListener('click', nextMarvel);
+window.onload = initializeMarvel;
 
-let index = 1 //start at the first movie
-
-function lastMarvel(){
-if(index>1 && index<41){ //prevents the user from going beyond the first & last movie
-    index--
-    getMarvel(index)
-}}
-
-function nextMarvel(){
-    if(index>0 && index<40){ 
-        index++
-        getMarvel(index)
-}}
-
-function getMarvel(){
-
-
-fetch(`https://mcuapi.herokuapp.com/api/v1/movies/${index}`)
-.then( res => res.json())
-    .then( data => {
-        console.log(data)
-        document.querySelector('img').src = data.cover_url
-        document.querySelector('.description').innerText = data.overview        
-        document.querySelector('.mTitle').innerText = data.title
-        document.querySelector('.release').innerText = `Release date: ${data.release_date}`
-        document.querySelector('.directed').innerText = `Directed by: ${data.directed_by}`
-        document.querySelector('iframe').src = data.trailer_url        
-        document.querySelector('.trailer').href = data.trailer_url        
-        document.querySelector('.trailer').innerText = `Watch the Trailer for ${data.title}`
-        document.querySelector('.imdb').innerText = `Explore ${data.title} on IMDB`
-        document.querySelector('.imdb').href = `https://www.imdb.com/title/${data.imdb_id}`
-
-});
+function initializeMarvel() {
+  fetch('https://mcuapi.herokuapp.com/api/v1/movies')
+    .then(res => res.json())
+    .then(data => {
+      maxIndex = data.total;
+      getMarvel(index);
+      updateButtonVisibility(); // Initialize button visibility
+    });
 }
 
+function updateButtonVisibility() {
+  // Hide back button if on first movie
+  backBtn.style.display = index <= 1 ? 'none' : 'block';
+  
+  // Hide forward button if on last movie
+  forwardBtn.style.display = index >= maxIndex ? 'none' : 'block';
+}
+
+function firstMarvel() {
+  if (index > 1) {
+    index = 1;
+    getMarvel(index);
+    updateButtonVisibility();
+  }
+}
+
+function latestMarvel() {
+  if (index < maxIndex) {
+    index = maxIndex;
+    getMarvel(index);
+    updateButtonVisibility();
+  }
+}
+
+function lastMarvel() {
+  if (index > 1) {
+    index--;
+    getMarvel(index);
+    updateButtonVisibility();
+  }
+}
+
+function nextMarvel() {
+  if (index < maxIndex) {
+    index++;
+    getMarvel(index);
+    updateButtonVisibility();
+  }
+}
+
+function getMarvel(movieIndex) {
+  fetch(`https://mcuapi.herokuapp.com/api/v1/movies/${movieIndex}`)
+    .then(res => res.json())
+    .then(data => {
+      // Update DOM with movie data
+      document.querySelector('img').src = data.cover_url;
+      document.querySelector('.description').innerText = data.overview;
+      document.querySelector('.mTitle').innerText = data.title
+      document.querySelector('.release').innerText = `Release date: ${data.release_date}`
+      document.querySelector('.directed').innerText = `Directed by: ${data.directed_by}`
+      document.querySelector('iframe').src = data.trailer_url        
+      document.querySelector('.trailer').href = data.trailer_url        
+      document.querySelector('.trailer').innerText = `Watch the Trailer for ${data.title}`
+      document.querySelector('.imdb').innerText = `Explore ${data.title} on IMDB`
+      document.querySelector('.imdb').href = `https://www.imdb.com/title/${data.imdb_id}`
+    });
+}
